568e90f631761254870846465b9d42f22616b1aa
Sneha Kumari <skumari2401a@gmail.com>
2026-09-23 19:19:30 +0530

    Time: 11 ms (66.67%), Space: 32.1 MB (67.01%) - LeetHub

--- 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        int ans=0;
-for(int x=1;x<=100;x++){
-int count=0;
-vector<int> position;
-for(int i=0;i<nums.size();i++){
-if(nums[i]==x){
-count++;
-position.push_back(i);
-}
-}
-if(count==3 && position[1]-position[0]==position[2]-position[1])
-ans++;
-}
-return ans;
+        vector<vector<int>> pos(101);
+        for (int i = 0; i < nums.size(); i++)
+            pos[nums[i]].push_back(i);
+        int ans = 0;
+        for (int x = 1; x <= 100; x++) {
+            if (pos[x].size() == 3 &&
+                pos[x][1] - pos[x][0] == pos[x][2] - pos[x][1])
+                ans++;
+        }
+        return ans;
     }
 };
